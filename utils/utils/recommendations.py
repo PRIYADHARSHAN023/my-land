bcc6735425a585b0f59bf3190ace78f4dfa02815
priyan023 <priyadharshan023@gmail.com>
2025-07-27 21:09:28 +0530

    Delete utils/utils directory

--- utils/utils/recommendations.py
+++
@@ -1,8 +0,0 @@
-# utils/recommendations.py
-
-def get_crop_recommendations(soil_type, rainfall, temperature):
-    # dummy logic
-    return ["Rice", "Wheat", "Maize"]
-
-def get_optimization_suggestions(crop, region):
-    return f"Use drip irrigation for {crop} in {region} region"
