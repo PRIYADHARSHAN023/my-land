9080d6ac9de95e38de5eb88df58c7a68871239f4
priyan023 <priyadharshan023@gmail.com>
2025-07-27 21:09:12 +0530

    Create recommendations.py

---
+++ utils/utils/recommendations.py
@@ -0,0 +1,8 @@
+# utils/recommendations.py
+
+def get_crop_recommendations(soil_type, rainfall, temperature):
+    # dummy logic
+    return ["Rice", "Wheat", "Maize"]
+
+def get_optimization_suggestions(crop, region):
+    return f"Use drip irrigation for {crop} in {region} region"
